0ff19610e64e767d0e47adc8487c8524a1c02594
shaikhajiratajanum <87138040+shaikhajiratajanum@users.noreply.github.com>
2021-07-22 20:49:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,75 +1,36 @@
-var garden,rabbit;
-var gardenImg,rabbitImg;
-var apple,appleImg;
-var leaf,leafImg;
+var shipImg,ship,seaImg,sea;
 function preload(){
-  gardenImg = loadImage("garden.png");
-  gardenImg=addImg("garden",gardenImg);
-  rabbitImg = loadImage("rabbit.png");
-  rabbitImg=addImg("rabbit",rabbitImg);
-  appleImg = loadImage("apple.png");
-  appleImg=addImg("apple",appleImg);
-  leafImg = loadImage("leaf.png");
-  leafImg=addImg("leaf",leafImg);
-  grassImg=loadImage("grass.png");
-  grassImg=addImg("grass",grassImg);
-  orangeLeafImg=loadImage("orangeLeaf.png");
-  orangeLeafImg=addImg("orangeleaf",orangeLeafImg);
+
+shipImg=loadAnimation("ship-1.png","ship-2.png");
+seaImage=loadImage("sea.png");
+
 }
 
 function setup(){
-  
-  createCanvas(600,600);
-  
-// Moving background
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
+  createCanvas(300,300);
 
-//creating boy running
-rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.09;
-rabbit.addImage(rabbitImg);
+   ship=createSprite(70,180,20,20);
+   ship.addAnimation("ship",shipImg);
+ship.scale=0.2;
+sea=createSprite(10,200,600,10);
+sea.x=sea.width/2;
+sea.velocityX=-2;
+sea.visible=false
 }
-
-
 function draw() {
-  background(0);
-  
-  edges= createEdgeSprites();
-  rabbit.collide(edges);
-  rabbit.X=world.mouseX
-  var select_sprites = Math.round(random(1,3));
-  if(frameCount%80===0){
-  }
-  if(select_sprites===1)
-{ createApple();}
- else if (select_sprites===2){                                                                 createApple();}
-else{  createApple();} 
- 
-  drawSprites();
 
-var select_sprites = Math.round(random(1,3));
-if (frameCount%100===0){
-if(select_sprites===1)
-{createLeaves();}
-else if(select_sprites===5){
-    createLeaves();}
-else {  createLeaves();  }}
+
+  background(seaImage);
+ if(keyDown("space")){
+   ship.velocityY=-10;
+ }
+
+ship.velocityY = ship.velocityY + 0.8
+
+if(sea.x < 0){
+  sea.x = sea.width/2
+}
+
+ship.collide(sea);
 drawSprites();
-}
-function createApple(){
-apple=createSprite(random(50,350),40,10,10)
-apple.addImg(appleImg);
-apple.scale=0.1;
-apple.velocityY=5;
-apple.lifetime=150;
-}
-function createLeaves(){
-leaf=createSprite(random(50,350),40,10,10);
-leaf.addImg(leafImg)
-leaf.scale=0.1;
-leaf.velocityY=5;
-leaf.lifetime=150;
-}
-drawSprite();{
 }
